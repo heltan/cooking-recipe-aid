e8da19e016be1c906da7590468cc3f4a8d9d6ae9
Helena Tanubrata <helen.tanubrata@sfcm.edu>
2021-12-06 19:28:27 -0800

    remove console logs

--- src/all-recipes.js
+++ src/all-recipes.js
@@ -13,8 +13,7 @@ function AllRecipes (setCurrentDish) {
     //use effect will be default look for something if its changed, make it dependant on something that wont change like empty array.
     //or it will loop forever
     useEffect(()=> {
-        console.log('all recipes current dish', setCurrentDish)
-        console.log('all recipes being called')
+
         findAllRecipes();
         
     }, [])
@@ -22,25 +21,22 @@ function AllRecipes (setCurrentDish) {
     function findAllRecipes () {
         axios.get('http://localhost:3000/findAll')
         .then(response=> {
-            console.log(
-                'set recipe list', response.data
-            )
+
             setRecipeList(response.data);
             renderRecipes(response.data);
         })
     }
     function renderRecipes (list) {
 
-        console.log('recipe list in render recipes', list)
         let recipeArray = [];
         for (let id in list) {
-            console.log('id?', list[id])
+     
             recipeArray.push(<div class = "recipes" id= {list[id].title + list[id].author}>
                 <h4>{list[id].title}</h4>
                 <nobr> by {list[id].author}</nobr> 
                 <button onClick = {(e)=> {
                     let dish = list[id];
-                    console.log('dish id', dish)
+                
                     //set that as currently cooking
                     // setCurrentDish(dishID)
                     setCurrentRecipe(dish)
